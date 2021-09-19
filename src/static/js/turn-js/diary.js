d142434eda9070df20de9c5e8dcffff22054c346
Honda Ichigo <honda.ichigo1515@gmail.com>
2021-09-20 07:28:20 +0900

    ADD: turn-jsの追加

---
+++ src/static/js/turn-js/diary.js
@@ -0,0 +1,101 @@
+//	ページを追加する
+function addPage(page, diary)
+{
+	let element = $('<div />', {});
+
+	if( diary.turn('addPage', element, page) )
+	{
+		if( page < 28 )
+		{
+			element.html('<div class="gradient"></div><div class="loader"></div>');
+			loadPage(page);
+		}
+	}
+}
+
+//	タブ
+function updateTabs()
+{
+	let tabs = {2: 'top'};
+	let left = [];
+	let right = [];
+	let diary = $('.diary');
+	let actualPage = diary.turn('page');
+	let view = diary.turn('view');
+
+	for (var page in tabs) {
+		let isHere = $.inArray(parseInt(page, 10), view) != -1;
+
+		if (page>actualPage && !isHere)
+			right.push('<a href="#page/' + page + '">' + tabs[page] + '</a>');
+		else if (isHere) {
+
+			if (page%2===0)
+				left.push('<a href="#page/' + page + '" class="on">' + tabs[page] + '</a>');
+			else
+				right.push('<a href="#page/' + page + '" class="on">' + tabs[page] + '</a>');
+		} else
+			left.push('<a href="#page/' + page + '">' + tabs[page] + '</a>');
+
+	}
+
+	$('.diary .tabs .left').html(left.join(''));
+	$('.diary .tabs .right').html(right.join(''));
+
+}
+
+
+function numberOfViews(diary)
+{
+	return diary.turn('pages') / 2 + 1;
+}
+
+
+function getViewNumber(diary, page) {
+	return parseInt((page || diary.turn('page'))/2 + 1, 10);
+}
+
+
+function moveBar(yes) {
+	if (Modernizr && Modernizr.csstransforms) {
+		$('#slider .ui-slider-handle').css({zIndex: yes ? -1 : 10000});
+	}
+}
+
+function setPreview(view) {
+
+	var previewWidth = 115,
+		previewHeight = 73,
+		previewSrc = '/static/im/preview.jpg',
+		preview = $(_thumbPreview.children(':first')),
+		numPages = (view==1 || view==$('#slider').slider('option', 'max')) ? 1 : 2,
+		width = (numPages==1) ? previewWidth/2 : previewWidth;
+
+	_thumbPreview.
+		addClass('no-transition').
+		css({width: width + 15,
+			height: previewHeight + 15,
+			top: -previewHeight - 30,
+			left: ($($('#slider').children(':first')).width() - width - 15)/2
+		});
+
+	preview.css({
+		width: width,
+		height: previewHeight
+	});
+
+	if (preview.css('background-image')==='' ||
+		preview.css('background-image')=='none') {
+
+		preview.css({backgroundImage: 'url(' + previewSrc + ')'});
+
+		setTimeout(function(){
+			_thumbPreview.removeClass('no-transition');
+		}, 0);
+
+	}
+
+	preview.css({backgroundPosition:
+		'0px -'+((view-1)*previewHeight)+'px'
+	});
+}
