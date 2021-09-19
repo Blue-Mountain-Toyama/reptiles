b9a2cc93e3b82650612a695c3e478ab7d3a4375a
Honda Ichigo <honda.ichigo1515@gmail.com>
2021-09-20 07:28:55 +0900

    UPDATE: テンプレートとスタイルの更新

--- src/top/views.py
+++ src/top/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def index(request):
-	params = {
-		"msg": "Hello, world",
-	}
+	params = {}
 	return render(request, 'top/index.html', params)
